2e82604b9fb9fa331b6b99c89f2ed3adb74e2628
Stone <569812422@qq.com>
2024-04-22 09:29:36 +0000

    4

--- src/task/prototypes/creep.ts
+++ src/task/prototypes/creep.ts
@@ -1,7 +1,27 @@
+import { initTask } from "task/initTask"
 
 export class CreepExtension extends Creep {
 
-    isIdle():boolean {
-        return false
+
+    taskGetter():ITask | null{
+        if(!this.task){
+            let protoTask = this.memory.task
+            this._task = protoTask ? initTask(protoTask) : null
+        }
+        return this._task
+    }
+    taskSetter(task:ITask | null){
+        this.memory.task = task ? task.proto:null
+    }
+    hasValidTaskGetter():boolean{
+        return !!this.task && this.task.isValid()
+    }
+    isIdleGetter():boolean {
+        return !this.hasValidTask
+    }
+    run(){
+        if(this.task){
+            return this.task.run()
+        }
     }
 }
